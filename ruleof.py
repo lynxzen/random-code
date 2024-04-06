5793e8996f77e783a0e2ea25f4465f5a59c783d1
lynxzen <joshuavargasjnv@gmail.com>
2024-04-06 14:57:04 -0700

    documenting and small other small changes

--- ruleof.py
+++ ruleof.py
@@ -2,16 +2,19 @@
 Quick implementation of the rule of (NUMBER) in cellular automata
 '''
 
+# simple decimal to binary converting function
 def dec_to_bin(dec):
     binary = bin(dec)
     binary = binary[2:]
     binary = fill_zeros(binary)
     return binary
 
+# helper function to dec_to_bin to ensure binary is 8-bit
 def fill_zeros(binary):
     num_zeros = 8 - len(binary)
     return ("0" * num_zeros) + binary
 
+# returns a dictionary the rule of (number) rules
 def make_rules(number):
     patterns = ["111", "110", "101", "100", "011", "010", "001", "000"]
     rules = {}
@@ -22,26 +25,28 @@ def make_rules(number):
             rules[patterns[idx]] = 0
     return rules
 
+# returns a string representing the 3-bit block where the adiabatic boundary condition is applied
 def adiabatic_boundary(row, isStart):
     if (isStart):
         return row[0] + row[0] + row[1]
     else:
         return row[-2] + row[-1] + row[-1]
 
+# returns a string representing the 3-bit block where reflection boundary condition is applied
 def reflection_boundary(row, isStart):
     if (isStart):
         return row[1] + row[0] + row[1]
     else:
         return row[-2] + row[-1] + row[-2]
 
+# displays the wolfram number and chart with the generated rules
 def draw_rules(start_row, rules, boundary_condition):
     print(start_row)
-    cols = len(start_row)
-    rows = cols
+    rows = len(start_row)
 
     for _ in range(rows):
         new_row = ""
-        for idx, i in enumerate(start_row):
+        for idx, _ in enumerate(start_row):
             # apply boundary condition at start
             if idx == 0:
                 curr_block = boundary_condition(start_row, 1)
